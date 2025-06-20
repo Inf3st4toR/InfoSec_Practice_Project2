ba6fe731b86521035fe448e5565fd750ce5dd2bd
inf3st4tor <xcharaix@gmail.com>
2025-06-20 04:42:20 +0000

    Completed 2nd test project

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,121 @@
-'use strict';
+"use strict";
+const { ObjectId } = require("mongodb");
 
-module.exports = function (app) {
-  
-  app.route('/api/threads/:board');
-    
-  app.route('/api/replies/:board');
+module.exports = function (app, collec) {
+  app
+    .route("/api/threads/:board")
+    .post(async (req, res) => {
+      const { text, delete_password } = req.body;
+      const created_time = new Date();
+      const result = await collec.insertOne({
+        board: req.params.board,
+        text: text,
+        created_on: created_time,
+        bumped_on: created_time,
+        reported: false,
+        delete_password: delete_password,
+        replies: [],
+      });
+      const object_Id = await collec.findOne({ _id: result.insertedId });
+      return res.json(object_Id);
+    })
+    .get(async (req, res) => {
+      const thread = await collec
+        .find(
+          { board: req.params.board },
+          { projection: { reported: 0, delete_password: 0 } }
+        )
+        .sort({ bumped_on: -1 })
+        .limit(10)
+        .toArray();
+      if (!thread.length)
+        return res.json({ error: "This thread doesn't exist." });
+      thread.forEach((obj) => {
+        obj.replies = (obj.replies || [])
+          .sort((a, b) => b.created_on - a.created_on)
+          .slice(0, 3)
+          .map(({ reported, delete_password, ...rest }) => rest);
+      });
+      return res.json(thread);
+    })
+    .put(async (req, res) => {
+      const { thread_id } = req.body;
+      const search = await collec.findOneAndUpdate(
+        { _id: new ObjectId(thread_id) },
+        { $set: { reported: true } },
+        { returnDocument: "after" }
+      );
+      if (search) return res.send("reported");
+      else return res.send({ error: "The ID is not found" });
+    })
+    .delete(async (req, res) => {
+      const { thread_id, delete_password } = req.body;
+      const result = await collec.deleteOne({
+        _id: new ObjectId(thread_id),
+        delete_password: delete_password,
+      });
+      if (result.deletedCount === 1) return res.send("success");
+      else return res.send("incorrect password");
+    });
 
+  app
+    .route("/api/replies/:board")
+    .post(async (req, res) => {
+      const { text, delete_password, thread_id } = req.body;
+      const update_time = new Date();
+      const reply = {
+        _id: new ObjectId(),
+        text: text,
+        created_on: update_time,
+        delete_password: delete_password,
+        reported: false,
+      };
+      const search = await collec.findOneAndUpdate(
+        { _id: new ObjectId(thread_id) },
+        { $set: { bumped_on: update_time }, $push: { replies: reply } },
+        { returnDocument: "after" }
+      );
+
+      return res.json(search || { error: "The ID is not found" });
+    })
+    .get(async (req, res) => {
+      const thread_id = req.query.thread_id;
+      const search = await collec.findOne(
+        { _id: new ObjectId(thread_id) },
+        { projection: { reported: 0, delete_password: 0 } }
+      );
+      if (search) {
+        search.replies = (search.replies || [])
+          .sort((a, b) => b.created_on - a.created_on)
+          .map(({ reported, delete_password, ...rest }) => rest);
+        return res.json(search);
+      } else return res.json({ error: "The ID is not found" });
+    })
+    .put(async (req, res) => {
+      const { thread_id, reply_id } = req.body;
+      const search = await collec.findOneAndUpdate(
+        { _id: new ObjectId(thread_id), "replies._id": new ObjectId(reply_id) },
+        { $set: { "replies.$.reported": true } },
+        { returnDocument: "after" }
+      );
+      if (search) return res.send("reported");
+      else return res.send({ error: "The ID is not found" });
+    })
+    .delete(async (req, res) => {
+      const { thread_id, reply_id, delete_password } = req.body;
+      const thread = await collec.findOne({ _id: new ObjectId(thread_id) });
+      const reply = thread.replies.find((r) =>
+        r._id.equals(new ObjectId(reply_id))
+      );
+      if (reply && reply.delete_password === delete_password) {
+        await collec.updateOne(
+          {
+            _id: new ObjectId(thread_id),
+            "replies._id": new ObjectId(reply_id),
+          },
+          { $set: { "replies.$.text": "[deleted]" } }
+        );
+        return res.send("success");
+      } else return res.send("incorrect password");
+    });
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,166 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
+const chaiHttp = require("chai-http");
+const chai = require("chai");
 const assert = chai.assert;
-const server = require('../server');
+const server = require("../server");
+const { MongoClient } = require("mongodb");
+const { ObjectId } = require("mongodb");
+const client = new MongoClient(process.env.MONGO_URI);
 
 chai.use(chaiHttp);
+let pool;
+const ids = [];
+let reply_id;
 
-suite('Functional Tests', function() {
+suite("Functional Tests", function () {
+  before(async function () {
+    await client.connect();
+    pool = client.db("Message_Board").collection("Threads");
+    const dummies = Array.from({ length: 11 }, (_, i) => ({
+      board: "func_tests",
+      text: `Dummy ${i + 1}`,
+      delete_password: "123",
+    }));
+    const dummiesDB = await pool.insertMany(dummies);
+    ids.push(
+      ...Object.values(dummiesDB.insertedIds).map((id) => id.toString())
+    );
+    const testRep = {
+      _id: new ObjectId(),
+      text: "Test Reply",
+      delete_password: "0000",
+    };
+    await pool.updateOne(
+      { _id: new ObjectId(ids[5]) },
+      { $push: { replies: testRep } }
+    );
+    reply_id = testRep._id.toString();
+  });
 
+  test("POST /api/threads/:board", function (done) {
+    chai
+      .request(server)
+      .post("/api/threads/func_tests")
+      .send({ text: "test thread", delete_password: "del" })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.property(res.body, "_id");
+        assert.equal(res.body.text, "test thread");
+        done();
+      });
+  });
+  test("GET /api/threads/:board", function (done) {
+    chai
+      .request(server)
+      .get("/api/threads/func_tests")
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.isArray(res.body);
+        assert.isAbove(res.body.length, 0);
+        assert.isAtMost(res.body.length, 10);
+        done();
+      });
+  });
+  test("DELETE /api/threads/:board wrong password", function (done) {
+    chai
+      .request(server)
+      .delete("/api/threads/func_tests")
+      .send({ thread_id: ids[0], delete_password: "234" })
+      .end(async function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, "incorrect password");
+        done();
+      });
+  });
+  test("DELETE /api/threads/:board", function (done) {
+    chai
+      .request(server)
+      .delete("/api/threads/func_tests")
+      .send({ thread_id: ids[0], delete_password: "123" })
+      .end(async function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, "success");
+        assert.isNull(await pool.findOne({ _id: new ObjectId(ids[0]) }));
+        ids.shift();
+        done();
+      });
+  });
+  test("PUT /api/threads/:board", function (done) {
+    chai
+      .request(server)
+      .put("/api/threads/func_tests")
+      .send({ thread_id: ids[0] })
+      .end(async function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, "reported");
+        const search = await pool.findOne({ _id: new ObjectId(ids[0]) });
+        assert.isTrue(search.reported);
+        done();
+      });
+  });
+  test("POST /api/replies/:board", function (done) {
+    chai
+      .request(server)
+      .post("/api/replies/func_tests")
+      .send({ text: "test reply", delete_password: "del", thread_id: ids[1] })
+      .end(async function (err, res) {
+        assert.equal(res.status, 200);
+        const search = await pool.findOne({ _id: new ObjectId(ids[1]) });
+        assert.equal(search.replies[0].text, "test reply");
+        done();
+      });
+  });
+  test("GET /api/replies/:board", function (done) {
+    chai
+      .request(server)
+      .get("/api/replies/func_tests")
+      .query({ thread_id: ids[1] })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.body._id, ids[1]);
+        assert.isArray(res.body.replies);
+        done();
+      });
+  });
+  test("DELETE /api/replies/:board wrong password", function (done) {
+    chai
+      .request(server)
+      .delete("/api/replies/func_tests")
+      .send({ thread_id: ids[4], reply_id: reply_id, delete_password: "0001" })
+      .end(async function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, "incorrect password");
+        done();
+      });
+  });
+  test("DELETE /api/replies/:board", function (done) {
+    chai
+      .request(server)
+      .delete("/api/replies/func_tests")
+      .send({ thread_id: ids[4], reply_id: reply_id, delete_password: "0000" })
+      .end(async function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, "success");
+        const search = await pool.findOne({ _id: new ObjectId(ids[4]) });
+        assert.equal(search.replies[0].text, "[deleted]");
+        done();
+      });
+  });
+  test("PUT /api/replies/:board", function (done) {
+    chai
+      .request(server)
+      .put("/api/replies/func_tests")
+      .send({ thread_id: ids[4], reply_id: reply_id })
+      .end(async function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, "reported");
+        const search = await pool.findOne({ _id: new ObjectId(ids[4]) });
+        assert.isTrue(search.replies[0].reported);
+        done();
+      });
+  });
+
+  after(async function () {
+    await pool.deleteMany({ board: "func_tests" });
+    await client.close();
+  });
 });
